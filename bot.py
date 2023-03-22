da62d18935257cb32fec6846d89b28d26717c7e4
richnanney <richardpnanney@gmail.com>
2023-03-22 14:41:51 -0600

    Code Formatting
    This one's for you, Cherry.

--- bot.py
+++ bot.py
@@ -5,6 +5,7 @@ import datetime
 
 DEFAULT_CHANNEL_ID = 488901798346620950
 
+
 async def send_message(content, channel):
     try:
         response = responses.handle_response(content)
@@ -13,20 +14,21 @@ async def send_message(content, channel):
     except Exception as e:
         print(e)
 
-def run_discord_bot():
 
+def run_discord_bot():
     TOKEN = open("discord_bot_token.txt", encoding="utf8").read()
     intents = discord.Intents.default()
     intents.message_content = True
     client = discord.Client(intents=intents)
-    
 
     async def schedule_daily_message():
-        last_checked  = ""
+        last_checked = ""
         await client.wait_until_ready()
         while not client.is_closed():
             now = datetime.datetime.now()
-            next_time = datetime.datetime(now.year, now.month, now.day, hour=9, minute=0, second=0)
+            next_time = datetime.datetime(
+                now.year, now.month, now.day, hour=9, minute=0, second=0
+            )
             wait_time = (next_time - now).total_seconds()
             await asyncio.sleep(wait_time)
             channel = client.get_channel(DEFAULT_CHANNEL_ID)
@@ -34,7 +36,7 @@ def run_discord_bot():
             if last_checked != now.day:
                 await send_message("!checkbirthday", channel)
                 last_checked = now.day
-        
+
     @client.event
     async def on_ready():
         print(f"{client.user} is now running!")
@@ -52,7 +54,9 @@ def run_discord_bot():
         server = str(message.guild.id)
         server_name = str(message.guild.name)
 
-        print(f"{username} said: '{user_message}' ({channel}) id: {id} server: {server} server_name: {server_name}") #This line prints out in the console every message in the servers that the bot is in. Comment it out if the bot is in a particularly busy server.
+        print(
+            f"{username} said: '{user_message}' ({channel}) id: {id} server: {server} server_name: {server_name}"
+        )  # This line prints out in the console every message in the servers that the bot is in. Comment it out if the bot is in a particularly busy server.
         if user_message:
             await send_message(user_message, message.channel)
 
--- responses.py
+++ responses.py
@@ -3,6 +3,7 @@ import calendar
 import json
 import random
 
+
 class Person:
     def __init__(self, name, year, month, day, pronoun):
         self.name = name
@@ -16,6 +17,7 @@ today = date.today()
 with open("cream_birthdays.json", "r") as f:
     people = json.loads(f.read(), object_hook=lambda d: Person(**d))
 
+
 def next_birthday():
     for person in people:
         if today.month <= int(person.month) and today.day < int(person.day):
@@ -23,7 +25,11 @@ def next_birthday():
             next_birthday_month = str(calendar.month_name[int(person.month)])
 
             ordinal = "th"
-            if str(person.day) == "11" or str(person.day)== "12" or str(person.day)== "13":
+            if (
+                str(person.day) == "11"
+                or str(person.day) == "12"
+                or str(person.day) == "13"
+            ):
                 ordinal = "th"
             elif str(person.day)[-1] == "1":
                 ordinal = "st"
@@ -31,11 +37,24 @@ def next_birthday():
                 ordinal = "nd"
             elif str(person.day)[-1] == "3":
                 ordinal = "rd"
-                    
-            message = "The next birthday is " + person.name + "'s, which will be on " + next_birthday_month + " " + person.day + ordinal + ". " + person.pronoun + " will be turning " + next_birthday_age + "."
+
+            message = (
+                "The next birthday is "
+                + person.name
+                + "'s, which will be on "
+                + next_birthday_month
+                + " "
+                + person.day
+                + ordinal
+                + ". "
+                + person.pronoun
+                + " will be turning "
+                + next_birthday_age
+                + "."
+            )
             return message
     else:
-          return "No more birthdays this year! Check again next year."
+        return "No more birthdays this year! Check again next year."
 
 
 def wish_birthday():
@@ -43,21 +62,29 @@ def wish_birthday():
     for person in people:
         if today.month == person.month and today.day == person.day:
             birthday_boy = person.name
-            message = "Wow @everyone! Today is our very own's " + birthday_boy + " birthday. Please wish them a happy birthday!"
+            message = (
+                "Wow @everyone! Today is our very own's "
+                + birthday_boy
+                + " birthday. Please wish them a happy birthday!"
+            )
             return message
     return False
+
 
 def list_birthdays():
     birthdays = ""
     for person in people:
-        message = person.name + "'s birthday is on " + person.month + "/" + person.day + ".\n"
+        message = (
+            person.name + "'s birthday is on " + person.month + "/" + person.day + ".\n"
+        )
         birthdays += message
     return birthdays
 
+
 def send_joke():
-    lines = open('jokes.txt', encoding="utf8").read().splitlines()
+    lines = open("jokes.txt", encoding="utf8").read().splitlines()
     myline = random.choice(lines)
-    myline = myline.replace('|','\n')
+    myline = myline.replace("|", "\n")
     return myline
 
 
@@ -65,7 +92,7 @@ def handle_response(message: str) -> str:
     p_message = message.lower()
     if p_message == "!nextbirthday":
         return next_birthday()
-    
+
     if p_message == "!help":
         return "`Beep boop I am a robo :)`"
 
@@ -74,11 +101,11 @@ def handle_response(message: str) -> str:
 
     if p_message == "!checkbirthday":
         return wish_birthday()
-    
+
     if p_message == "!listbirthdays":
         return list_birthdays()
-    
+
     if p_message == "!joke":
         return send_joke()
-    
+
     return False
